2c41bd425396ccbe79ae38506a9d9bbb897e9db5
Arcane-Cloud1 <mocloud4@163.com>
2026-02-05 08:18:49 +0800

    Optimize project architecture, code details, add ESLint. Improve project documentation

--- src/routes/captchaRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getSliderCaptcha, verifySliderCaptcha } from '../controllers/captchaController';
-
-const router = Router();
-
-router.get('/slider', getSliderCaptcha);
-router.post('/slider/verify', verifySliderCaptcha);
-
-export default router;
--- src/routes/configRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getPublicSiteConfig, getPublicConfig } from '../controllers/settingController';
-
-const router = Router();
-
-router.get('/public', getPublicConfig);
-router.get('/site', getPublicSiteConfig);
-
-export default router;
